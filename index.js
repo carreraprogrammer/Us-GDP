610a96d14350b84eccf049b63bb3f4e22c6c9e4f
Daniel <carreraprogrammer@gmail.com>
2023-06-25 08:51:50 -0500

    Add svg container

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 // test
+const width = 800;
+const height = 400;
+const margin = { top: 50, bottom: 50, left: 50, right: 50 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    let title = document.getElementById("title");
-    if (title) {
-      title.innerHTML = "TEST TITLE";
-    }
-  });
+const svg = d3.select('#bar-chart')
+              .append('svg')
+              .attr('height', height - margin.top - margin.bottom)
+              .attr('width', width - margin.left -margin.right)
+              .attr('viewBox', [0, 0, width, height])
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
   .then(response => response.json())
   .then(data => {
     console.log(data)
   })
+
+
