b5604122a4e34cd39c0e4746718c077da92cda31
Daniel <carreraprogrammer@gmail.com>
2023-06-25 17:54:35 -0500

    Add scale

--- index.js
+++ index.js
@@ -1,73 +1,76 @@
-const data = async () => {
-    const response = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json') .then((response) => response.json());
-    return response
-  };
+const drawChart = async () => {
+  const response = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+  const data = await response.json();
 
-const testArray = [['1947-01-01', 243.1], ['1947-04-01', 246.3], ['1947-07-01', 250.1], ['1947-01-01', 243.1], ['1947-04-01', 246.3], ['1947-07-01', 250.1]];
+  const width = 900;
+  const height = 400;
+  const padding = 30;
 
-const width = 800;
-const height = 400;
-const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+  const yScale = d3.scaleLinear()
+                   .domain([0, d3.max(data.data, (d) => d[1])])
+                   .range([height - padding, padding]);
 
-const svg = d3.select("#bar-chart")
-              .append("svg")
-              .attr("width", width)
-              .attr("height", height);
+  const xScale = d3.scaleLinear()
+                   .domain([0, data.data.length])
+                   .range([padding, width - padding])
 
-           svg.selectAll("rect")
-              .data(testArray)
-              .enter()
-              .append("rect")
-              .attr("x", (d, i) => i * 30)
-              .attr("y", (d, i) => height - d[1])
-              .attr("width", 25)
-              .attr("height", (d, i) => d[1])
-              .attr("fill", "red")
-              .attr("class", "bar");
+  const svg = d3.select("#bar-chart")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);
 
-            svg.selectAll("foreignObject")
-              .data(testArray)
-              .enter()
-              .append("foreignObject")
-              .attr("x", (d, i) => i * 10 + 50)
-              .attr("y", (d, i) => (height - d[1] - 100))
-              .attr("width", 200)
-              .attr("height", 80)
-              .html((d) => `<div class="text-container">${d[1]}</div>`)
-              .attr("class", "no-show");
+  svg.selectAll("rect")
+     .data(data.data)
+     .enter()
+     .append("rect")
+     .attr("x", (d, i) => xScale(i))
+     .attr("y", (d, i) => yScale(d[1]) + padding)
+     .attr("width", 3)
+     .attr("height", (d, i) => height - yScale(d[1]))
+     .attr("fill", "red")
+     .attr("class", "bar");
 
-            d3.select("svg")
-              .append("style")
-              .text(`
-                .text-container {
-                  height: 100%;
-                  width: 100%;
-                  border: 2px solid black;
-                  background-color: rgba(0, 0, 255, 0.149);
-                  color: black;
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  border-radius: 5px
-                }
-              `);
+  svg.selectAll("foreignObject")
+     .data(data.data)
+     .enter()
+     .append("foreignObject")
+     .attr("x", (d, i) => i * 2 + 5)
+     .attr("y", 170)
+     .attr("width", 200)
+     .attr("height", 80)
+     .html((d) => `<div class="text-container">${d[1]}</div>`)
+     .attr("class", "no-show");
 
-        
-let bars = document.getElementsByClassName("bar");
-let tags = document.getElementsByClassName("no-show");
+  d3.select("svg")
+    .append("style")
+    .text(`
+      .text-container {
+        height: 100%;
+        width: 100%;
+        border: 2px solid black;
+        background-color: rgba(0, 0, 255, 0.149);
+        color: black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px
+      }
+    `);
 
+  let bars = document.getElementsByClassName("bar");
+  let tags = document.getElementsByClassName("no-show");
 
-Array.from(bars).forEach((bar, index) => {
-  bar.addEventListener("mouseover", () => {
+  Array.from(bars).forEach((bar, index) => {
+    bar.addEventListener("mouseover", () => {
+      let tag = tags[index];
+      tag.classList.add("show");
+    });
 
-    let tag = tags[index];
-    tag.classList.add("show");
+    bar.addEventListener("mouseout", () => {
+      let tag = tags[index];
+      tag.classList.remove("show");
+    });
   });
+};
 
-  bar.addEventListener("mouseout", () => {
-
-    let tag = tags[index];
-    
-    tag.classList.remove("show");
-  });
-});
+drawChart();
